648d02f1df591507c8c6bc26b91fd79515b18773
Adarsh Malviya <adarshmalviya1199@gmail.com>
2023-04-02 10:37:43 +0530

    commit

--- src/components/SingelProduct.jsx
+++ src/components/SingelProduct.jsx
@@ -1,7 +1,51 @@
 import React from "react";
+import {Card, Button} from "react-bootstrap";
+import Rating from "./Rating";
 
 const SingelProduct = ({prod}) => {
- return <div>{prod.name}</div>;
+ return (
+  <div className="produts">
+   <Card>
+    <Card.Img className="productImage" variant="top" src={prod.image} alt="" />
+    <Card.Body>
+     <Card.Title>Rs. {prod.name}</Card.Title>
+     <Card.Subtitle style={{paddingBottom: 10}}>
+      <span>{prod.price.split(".")[0]}</span>
+      {prod.fastDelivery ? (
+       <div>Fast Delivery</div>
+      ) : (
+       <div> 4 day Delivery</div>
+      )}
+      <Rating rating={prod.rating} />
+     </Card.Subtitle>
+
+     <Button
+      variant="danger"
+      onClick={() =>
+       dispatch({
+        type: "REMOVE_FROM_CART",
+        payload: prod,
+       })
+      }
+     >
+      Remove from Cart
+     </Button>
+
+     <Button
+      onClick={() =>
+       dispatch({
+        type: "ADD_TO_CART",
+        payload: prod,
+       })
+      }
+      disabled={!prod.inStock}
+     >
+      {!prod.inStock ? "Out of Stock" : "Add to Cart"}
+     </Button>
+    </Card.Body>
+   </Card>{" "}
+  </div>
+ );
 };
 
 export default SingelProduct;
